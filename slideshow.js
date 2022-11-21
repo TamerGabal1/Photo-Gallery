4a8fd3fe69f0426d71bced1e113d546adc0289d7
Tamer Gabal <gabaltamer555@gmail.com>
2022-11-21 12:43:03 -0500

    worked on about

--- slideshow.js
+++ slideshow.js
@@ -23,11 +23,11 @@ function openMission(){
     header.style.display = "none";
     var subheader = document.getElementById("subheader");
     subheader.style.display = "none";
-    var button = document.getElementById("button");
+    var button = document.querySelector(".openButton");
     button.style.display = "none";
     var mission = document.getElementById("statement");
     mission.style.display = "block";
-    var button1 = document.getElementById("button1");
+    var button1 = document.querySelector(".closeButton");
     button1.style.display = "block";
     
 }
@@ -36,11 +36,37 @@ function closeMission(){
     header.style.display = "block";
     var subheader = document.getElementById("subheader");
     subheader.style.display = "block";
-    var button = document.getElementById("button");
+    var button = document.querySelector(".openButton");
     button.style.display = "block";
     var mission = document.getElementById("statement");
     mission.style.display = "none";
-    var button1 = document.getElementById("button1");
+    var button1 = document.querySelector(".closeButton");
     button1.style.display = "none";
 }
 
+function openAbout(){
+    console.log("clicked")
+    var header = document.getElementById("container2");
+    header.style.display = "none";
+    // var subheader = document.getElementById("subheader");
+    // subheader.style.display = "none";
+    var button = document.querySelector(".openButton");
+    button.style.display = "none";
+    var about = document.getElementById("us");
+    about.style.display = "grid";
+    var button1 = document.querySelector(".closeButton");
+    button1.style.display = "block";
+    
+}
+function closeAbout(){
+    var header = document.getElementById("container2");
+    header.style.display = "block";
+    // var subheader = document.getElementById("subheader");
+    // subheader.style.display = "block";
+    var button = document.querySelector(".openButton");
+    button.style.display = "block";
+    var about = document.getElementById("us");
+    about.style.display = "none";
+    var button1 = document.querySelector(".closeButton");
+    button1.style.display = "none";
+}
