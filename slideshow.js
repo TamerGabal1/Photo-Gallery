89ef2031c10d1a65a5811d680ed78ec6174d279f
Tamer Gabal <gabaltamer555@gmail.com>
2022-11-30 22:19:34 -0500

    changed pages
    Co-authored-by: PhilipEfraim <PhilipEfraim18@users.noreply.github.com>

--- slideshow.js
+++ slideshow.js
@@ -1,72 +1,76 @@
 function openFullImg(pic){
-    var fullImgBox =  document.getElementById("fullImgBox");
+    let fullImgBox =  document.getElementById("fullImgBox");
     fullImgBox.style.display = "flex";
-    var fullImg = document.getElementById("fullImg");
+    let fullImg = document.getElementById("fullImg");
     fullImg.src = pic;
 }
 function closeFullImg(){
-    var fullImgBox =  document.getElementById("fullImgBox");
+    let fullImgBox =  document.getElementById("fullImgBox");
     fullImgBox.style.display = "none";
 }
 function openFullImg1(pic){
-    var fullImgBox1 =  document.getElementById("fullImgBox1");
+    let fullImgBox1 =  document.getElementById("fullImgBox1");
     fullImgBox1.style.display = "flex";
-    var fullImg1 = document.getElementById("fullImg1");
+    let fullImg1 = document.getElementById("fullImg1");
     fullImg1.src = pic;
 }
 function closeFullImg1(){
-    var fullImgBox1 =  document.getElementById("fullImgBox1");
+    let fullImgBox1 =  document.getElementById("fullImgBox1");
     fullImgBox1.style.display = "none";
 }
 function openMission(){
-    var header = document.getElementById("container");
+    let header = document.getElementById("container");
     header.style.display = "none";
-    var subheader = document.getElementById("subheader");
+    let subheader = document.getElementById("subheader");
     subheader.style.display = "none";
-    var button = document.querySelector(".openButton");
+    let button = document.querySelector(".openButton");
     button.style.display = "none";
-    var mission = document.getElementById("statement");
+    let mission = document.getElementById("statement");
     mission.style.display = "block";
-    var button1 = document.querySelector(".closeButton");
-    button1.style.display = "block";
+    let button1 = document.querySelector(".closeButton");
+    button1.style.display = "flex";
     
 }
 function closeMission(){
-    var header = document.getElementById("container");
+    let header = document.getElementById("container");
     header.style.display = "block";
-    var subheader = document.getElementById("subheader");
+    let subheader = document.getElementById("subheader");
     subheader.style.display = "block";
-    var button = document.querySelector(".openButton");
-    button.style.display = "block";
-    var mission = document.getElementById("statement");
+    let button = document.querySelector(".openButton");
+    button.style.display = "flex";
+    let mission = document.getElementById("statement");
     mission.style.display = "none";
-    var button1 = document.querySelector(".closeButton");
+    let button1 = document.querySelector(".closeButton");
     button1.style.display = "none";
 }
 
 function openAbout(){
     console.log("clicked")
-    var header = document.getElementById("container2");
+    let header = document.getElementById("container2");
     header.style.display = "none";
-    // var subheader = document.getElementById("subheader");
+    // let subheader = document.getElementById("subheader");
     // subheader.style.display = "none";
-    var button = document.querySelector(".openButton");
+    let contact = document.getElementById("contactUs");
+    contact.style.display = "flex";
+    let button = document.querySelector(".openButton");
     button.style.display = "none";
-    var about = document.getElementById("us");
+    let about = document.getElementById("us");
     about.style.display = "grid";
-    var button1 = document.querySelector(".closeButton");
-    button1.style.display = "block";
+    let button1 = document.querySelector(".closeButton");
+    button1.style.display = "flex";
     
 }
 function closeAbout(){
-    var header = document.getElementById("container2");
+    let header = document.getElementById("container2");
     header.style.display = "block";
-    // var subheader = document.getElementById("subheader");
+    // let subheader = document.getElementById("subheader");
     // subheader.style.display = "block";
-    var button = document.querySelector(".openButton");
-    button.style.display = "block";
-    var about = document.getElementById("us");
+    let contact = document.getElementById("contactUs");
+    contact.style.display = "none";
+    let button = document.querySelector(".openButton");
+    button.style.display = "flex";
+    let about = document.getElementById("us");
     about.style.display = "none";
-    var button1 = document.querySelector(".closeButton");
+    let button1 = document.querySelector(".closeButton");
     button1.style.display = "none";
 }
